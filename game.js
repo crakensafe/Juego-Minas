7612e2ca101f85392ee0ff4ce59ac030963d6536
crakensafe <crakensafe@icloud.com>
2024-03-19 15:28:20 -0600

    creando las plantillas de mapas

---
+++ game.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    game.fillRect(0,0,400,150);
+
+}
---
+++ maps.js
@@ -0,0 +1,54 @@
+/*
+ * Reglas:
+ * El final de cada nivel debe ser el inicio del siguiente
+*/
+
+const emojis = {
+    '-': ' ',
+    'O': '🚪',
+    'X': '💣',
+    'I': '🎁',
+    'PLAYER': '💀',
+    'BOMB_COLLISION': '🔥',
+    'GAME_OVER': '👎',
+    'WIN': '🏆',
+    'HEART': '❤️',
+};
+
+const maps = [];
+maps.push(`
+    IX----XXXX
+    -XXXX---XX
+    -XXXXXX-XX
+    ------X-XX
+    XXXXX-----
+    XX----XXX-
+    XXXXX-XXX-
+    XX----XXX-
+    XX-XXXXXX-
+    O--XXXXXX-
+`);
+maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+`);
+maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+`);
